a93355676830623d4c2f8821ef67b9e4214a35f9
CBAmsterdam <info@iloki.nl>
2019-02-26 12:31:59 +0100

    fix

--- src/date_partition_util.py
+++ src/date_partition_util.py
@@ -20,7 +20,7 @@ class DatePartitionUtil(object):
     def week_partition(self, d=None):
         d = d if d is not None else self.today()
         start = datetime.strptime(
-            f"{d.year}_{self.week_number(d)}_1", "%Y_%W_%w")
+            f"{d.year}-{self.week_number(d)}-1", '%G-%V-%w').date()
         end = start + timedelta(7)
         return (start, end)
 
